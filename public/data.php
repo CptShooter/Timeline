cafec464c90af525c24e2d6f82f74dd16db67217
Paweł Kiełt <kieltpawel@gmail.com>
2023-06-13 20:05:46 +0200

    add new games

--- public/data.php
+++ public/data.php
@@ -7,4 +7,4 @@ require_once '../vendor/autoload.php';
 header('Content-Type: application/json');
 
 $data = new Data();
-echo $data->removeGamesOlderThan(15, 120)->sortGamesByDate()->buildJson()->getGamesToJson();
+echo $data->removeGamesOlderThan(15, 150)->sortGamesByDate()->buildJson()->getGamesToJson();
